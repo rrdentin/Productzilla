031d99ecce101858e32c2b38be1897f2ac8dfa5b
rrdentin <denti.dona@gmail.com>
2024-11-01 16:52:20 +0700

    update

--- [Task 4] Express JS, MongoDB, Swagger/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Importing express
-const router = require("express").Router();
-const authRoutes = require("./auth");
-const bookRoutes = require("./book");
-router.use("/", authRoutes);
-router.use("/books", bookRoutes);
-module.exports = router;
